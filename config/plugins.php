ef25d40ce4b2637535e9b240c81f50b41dcbede5
fly <fly@load-page.com>
2017-08-10 21:09:06 +0800

    add user space plugins.php

---
+++ config/plugins.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+	'attachment' => [
+		'enable' => true,
+	],
+	'catalog' => [
+		'enable' => true,
+	],
+	'manual' => [
+		'enable' => true,
+	],
+	'system' => [
+		'enable' => true,
+	],
+	'tools' => [
+		'enable' => true,
+	],
+	'wechat' => [
+		'enable' => true,
+	],
+];
